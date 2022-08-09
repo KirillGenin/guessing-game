d5aed1b18de384098cab897d15002913b68dfbcb
KirillGenin <332211op@gmail.com>
2022-08-09 13:43:31 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.num = Math.ceil( (this.min + this.max) / 2 )
+        return this.num
     }
 
     lower() {
-
+        this.max = this.num
     }
 
     greater() {
-
+        this.min = this.num
     }
 }
 
